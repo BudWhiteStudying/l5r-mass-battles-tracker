75bf6a683a6c79787300f911fddae1ea477d9f85
Tommaso Meledina <tommaso.meledina@it.ibm.com>
2021-05-27 01:23:34 +0200

    feat: set up Angular routing and stub pages

--- mass-battle-tracker-client/src/main/angular/src/app/app-routing.module.ts
+++ mass-battle-tracker-client/src/main/angular/src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing/landing.component';
+import { FinalSummaryComponent } from './new-battle/final-summary/final-summary.component';
+import { InvolvedArmiesComponent } from './new-battle/involved-armies/involved-armies.component';
+import { NameDescriptionComponent } from './new-battle/name-description/name-description.component';
+import { AvailableBattlesComponent } from './resume-battle/available-battles/available-battles.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: 'new-battle/name-description',
+    component: NameDescriptionComponent
+  },
+  {
+    path: 'new-battle/involved-armies',
+    component: InvolvedArmiesComponent
+  },
+  {
+    path: 'new-battle/final-summary',
+    component: FinalSummaryComponent
+  },
+  {
+    path: 'resume-battle/available-battles',
+    component: AvailableBattlesComponent
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mass-battle-tracker-client/src/main/angular/src/app/app.module.ts
+++ mass-battle-tracker-client/src/main/angular/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingModule } from './landing/landing.module';
+import { NewBattleModule } from './new-battle/new-battle.module';
+import { ResumeBattleModule } from './resume-battle/resume-battle.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LandingModule,
+    NewBattleModule,
+    ResumeBattleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
