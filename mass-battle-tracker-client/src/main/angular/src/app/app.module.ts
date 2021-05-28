f889d23f0a7486924c6476ee337db9428be8218e
Tommaso Meledina <tommaso.meledina@it.ibm.com>
2021-05-28 02:18:37 +0200

    feat: implement landing page leveraging Angular Material

--- mass-battle-tracker-client/src/main/angular/src/app/app.module.ts
+++ mass-battle-tracker-client/src/main/angular/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { LandingModule } from './landing/landing.module';
 import { NewBattleModule } from './new-battle/new-battle.module';
 import { ResumeBattleModule } from './resume-battle/resume-battle.module';
@@ -16,6 +19,7 @@ import { ResumeBattleModule } from './resume-battle/resume-battle.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     LandingModule,
     NewBattleModule,
     ResumeBattleModule
